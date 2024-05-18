833ebaac34d2be80b133f441aad2f10dbbab5167
Daiki-Ishiyama <daikiishiyama@keio.jp>
2024-05-19 07:46:10 +0900

    Initial Commit

---
+++ Map_App/Map_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_AppApp.swift
+//  Map_App
+//
+//  Created by daiki ishiyama on 2024/05/19.
+//
+
+import SwiftUI
+
+@main
+struct Map_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
